6ecf9edc0a3d5779e7c266114742e73f7432135f
SwapnilPadekar <slpadekar29@gmail.com>
2024-06-24 16:57:19 +0530

    Lab267 added

--- src/test/java/com/thetestingacademy/testng/Lab267.java
+++ src/test/java/com/thetestingacademy/testng/Lab267.java
@@ -1,5 +1,9 @@
 package com.thetestingacademy.testng;
 
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import sun.security.rsa.RSAUtil;
+
 public class Lab267 {
 
     //CRUD
@@ -11,5 +15,39 @@ public class Lab267 {
 
     String token;
     Integer bookingID;
+
+    public String getToken(){
+        token = "123";
+        return token;
+    }
+
+    @BeforeTest
+    public void getTokenAndBookingID(){
+        token = getToken();
+        //Post request
+        //Post code
+        bookimngID = 123;
+    }
+
+    @Test
+    public void testPUTReq(){
+        System.out.println(token);
+        System.out.println(bookingID);
+        //PUT code
+    }
+
+    @Test
+    public void testDELETEReq(){
+        System.out.println(token);
+        System.out.println(bookingID);
+        //DELETE code
+    }
+
+    @Test
+    public void testGETReq(){
+        System.out.println(bookingID);
+        //GET code
+    }
+
 
 }
